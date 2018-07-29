a0e8ca7d5d6d882d248fc70784fd16ff34b7c8b5
mazarag2 <mazarag2@asu.edu>
2018-07-29 13:37:32 -0700

    Add Travis CI and test cases

--- src/DarkSoulsStrategy.js
+++ src/DarkSoulsStrategy.js
@@ -20,23 +20,20 @@ var DarkSoulsStrategy = function(){
 			}
 				
 		*/
-	
-			//var ids = [];
+
 		for(var x = 0; x <= param.length - 1; x++){
 			
 			var id = param[x].id;
 			console.log(id);
 			if(id == 12 || id == 25 || id == 31){
 
-				return "yea dude since "  + gameName + "is an RPG with Hack and Slash Elements and exploration its basically Dark Souls";
+				return "yea dude since "  + gameName + " is an RPG with Hack and Slash Elements and exploration its basically Dark Souls";
 				
 			}
 			
 		}
 		
-		return "yea its nothing like Dark Souls";
-
-
+		return "nah its nothing like Dark Souls";
 
 	}
 }
--- test/Artoriastest.js
+++ test/Artoriastest.js
@@ -3,46 +3,47 @@ const Util = require("../src/Util");
 const ds = require("../src/DarkSoulsStrategy");
 const dotev = require('dotenv').config();
 var expect = require('chai').expect;
-
+var strategy = null;
 describe('Util', function() {
-  describe('#CheckUtilFunctions()', function() {
-	/*
-	it('should return a max number for a list',function(done){
-		
-	
-		
-		var util = new Util();
-		
-		var list = [ { id: 740, genres: [ 5 ] },
-					  { id: 45149, genres: [ 5, 31 ] },
-					  { id: 986, genres: [ 5 ] },
-					  { id: 3122, genres: [ 5 ] },
-					  { id: 43955, genres: [ 5 ] },
-					  { id: 987, genres: [ 5 ] },
-					  { id: 45147, genres: [ 5, 31 ] },
-					  { id: 988, genres: [ 11, 15 ] },
-					  { id: 43956, genres: [ 11, 15 ] },
-					  { id: 989, genres: [ 5 ] } ];
-
-		
-		var comNum = util.findCommonId(list);
-		
-		expect(comNum).to.be(5);
-	});	
-	*/
 	
 	describe('#CheckDarkSoulsStrategy()', function(){
 		
-		var strategy = new ds();
+		before(function(){
+			// runs before all tests in this block(need to log in a test user to authenticate Fb Calls)
+			strategy = new ds();
+			
+		});
+		it('should return a negative message when its not DarkSouls',function(){
+			
+			//var strategy = new ds();
 		
+			var params = [{id : 6, slug: 'Fighting'}];
+			
+			var gameName = "Tekken";
+			
+			var result = strategy.isitLikeDarkSouls(params,gameName);
+			
+			expect(result).to.equal("nah its nothing like Dark Souls");
+		});
 		
-		
+		it('should return a positive message when its Dark Souls',function(){
+			
+			
+			var params = [{id : 12, slug: 'Role-Playing-Game'}];
+			
+			var gameName = "Skyrim";
+			
+			var result = strategy.isitLikeDarkSouls(params,gameName);
+			
+			expect(result).to.equal("yea dude since "  + gameName + " is an RPG with Hack and Slash Elements and exploration its basically Dark Souls");
+	
+			
+		});
 		
 		
 	});
 	
 	
-	
-  });
+
 
 });
